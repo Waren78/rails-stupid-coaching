8c17b6a3b52f80c9a82b725e2bb383e9d2d7d18e
Erwan RAHIM <waren78.krahim@gmail.com>
2024-01-26 18:22:03 +0100

    stupid coaching no bootstrap

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = calculate_answer
+  end
+
+  private
+
+  def calculate_answer
+    if params["question"] == "I am going to work"
+      "Great!"
+    elsif params["question"].include?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
